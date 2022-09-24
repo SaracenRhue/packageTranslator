25504d824362eecef9ea9c5d3d90a995b8153183
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-24 17:30:53 +0200

    fix

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mkdir sr
-cd sr
+mkdir sr_data
+cd sr_data
 wget https://raw.githubusercontent.com/SaracenRhue/packageTranslator/main/sr/config.yml
 wget https://github.com/SaracenRhue/packageTranslator/blob/main/sr/main.py
 wget https://github.com/SaracenRhue/packageTranslator/blob/main/sr/setup.py
 cd
 
-python3 sr/setup.py
-rm -rf sr/setup.py
+python3 sr_data/setup.py
+rm -rf sr_data/setup.py
 
 echo "alias sr='python3 sr/main.py'" >> ~/.zshrc
 echo "alias sr='python3 sr/main.py'" >> ~/.bashrc
