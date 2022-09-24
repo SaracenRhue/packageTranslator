9416369de02d7c28054eefb5d3955fb3c395d12e
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-24 17:42:21 +0200

    upd

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 mkdir sr_data
 cd sr_data
 wget https://raw.githubusercontent.com/SaracenRhue/packageTranslator/main/sr/config.yml
-wget https://github.com/SaracenRhue/packageTranslator/blob/main/sr/main.py
-wget https://github.com/SaracenRhue/packageTranslator/blob/main/sr/setup.py
+wget https://raw.githubusercontent.com/SaracenRhue/packageTranslator/main/sr/main.py
+wget https://raw.githubusercontent.com/SaracenRhue/packageTranslator/main/sr/setup.py
 cd
 
 python3 sr_data/setup.py
