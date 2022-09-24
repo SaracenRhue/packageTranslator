67199a681614bc4926531224d414cf4c744369bd
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-24 17:18:44 +0200

    upd

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # packageTranslator
+
+## Installation
+
+```bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/SaracenRhue/packageTranslator/main/install.sh)"
+```
+
+You can change the package manager at the bottom of the `config.yml` file.
--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@
 
 mkdir sr
 cd sr
-wget
-wget
-wget
+wget https://raw.githubusercontent.com/SaracenRhue/packageTranslator/main/sr/config.yml
+wget https://github.com/SaracenRhue/packageTranslator/blob/main/sr/main.py
+wget https://github.com/SaracenRhue/packageTranslator/blob/main/sr/setup.py
 cd
 
 python3 sr/setup.py
+rm -rf sr/setup.py
 
 echo "alias sr='python3 sr/main.py'" >> ~/.zshrc
 echo "alias sr='python3 sr/main.py'" >> ~/.bashrc
